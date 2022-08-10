6d26153f9e20c67e8a3cc821f2fdf9c5ba080e4a
Nguyễn Thị Thanh Hoan <nguyenthithanhhoan@Thanh-hoan-2.local>
2022-08-10 15:11:28 +0700

    average

---
+++ Datatype/aver.js
@@ -0,0 +1,11 @@
+
+function showSum() {
+    let physical = parseInt(document.getElementById('physical').value);
+    let chemistry = parseInt(document.getElementById('chemistry').value);
+    let biological = parseInt(document.getElementById('biological').value);
+    let sum = physical + chemistry + biological;
+    document.getElementById("sum").innerHTML = sum;
+console.log(sum);
+}
+// }
+
