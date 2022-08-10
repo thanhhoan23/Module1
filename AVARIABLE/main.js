d719dece1a37fa40f905b6d7112d883ab8acf490
Nguyễn Thị Thanh Hoan <nguyenthithanhhoan@Thanh-hoan-2.local>
2022-08-10 11:25:01 +0700

    avariable

---
+++ AVARIABLE/main.js
@@ -0,0 +1,12 @@
+        let i = 10;
+        let f = 20.5;
+        let b = true;
+        let s = 'Ha noi'
+
+        document.write('i=' +i); 
+        document.write("<br>");
+        document.write('f=' +0.5);
+        document.write("<br>");
+        document.write("b=" + true);
+        document.write("<br>")
+        document.write("s=" + "Ha noi")
---
+++ Javasript/main.js
@@ -0,0 +1,4 @@
+let a = 15;
+let b = 17;
+let c = 110;
+console.log(`${a}^2+ ${b}^2 = (${a}+${b}`)
